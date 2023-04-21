60761f338cde6f4a5d7c62857b014848a40bf8bd
Fishead_East <91858912+Antony-Zhang@users.noreply.github.com>
2023-04-21 18:34:02 +0800

    简单改名

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
+  title: '稻草人自留地',
+  author: 'Fishead_East',
+  email: 'ytzd2696@foxmail.com',
   link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
+  description: '一隅静土，稻草人凝望的。',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
@@ -52,7 +52,7 @@ const BLOG = {
     },
     cusdisConfig: {
       appId: '', // data-app-id
-      host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
+      host: 'https://cusdis.com', // djiat, change this if you're using self-hosted version
       scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
     }
   },
